901b4075a52360fac7cca6421d7dda3106808afc
bindu <davuluribindu995@gmail.com>
2025-05-19 14:30:03 +0530

    commit

--- hello.sh
+++ hello.sh
@@ -5,5 +5,5 @@ echo "hello shell scripting"
 person1=bindu
 person2=mani
 
-echo "hello $person1 what are u doing here
+echo "hello $person1 what are u doing here"
 echo "hi $person2 i am waiting for my frd"
