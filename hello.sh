1465e9a437609d50fa4dd1b6cc1a09fee74d06a2
bindu <davuluribindu995@gmail.com>
2025-05-21 12:08:38 +0530

    allow all commands

--- hello.sh
+++ hello.sh
@@ -7,3 +7,7 @@ person2=mani
 
 echo "hello $person1 what are u doing here"
 echo "hi $person2 i am waiting for my frd"
+ echo "all variable passed to script $@"
+ echo "varables passed $#"
+ echo script name $0
+ echo present working directory $pwd
