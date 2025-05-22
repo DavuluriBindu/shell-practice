7bce02b58960b39150bbf6286d927678c9a8d61f
bindu <davuluribindu995@gmail.com>
2025-05-22 12:24:42 +0530

    allow all changes

--- install-command.sh
+++ install-command.sh
@@ -10,12 +10,12 @@ else
      echo "you are running with root access"
 fi
 
-dnf install mysqlhjfjsdy -y
+dnf install mysqlgsdfwe -y
 
 if [ $? -eq 0 ]
 then 
     echo "installation nysql is ... success"
 else
     echo "installation mysql is ... failure"
-    exit 1
+    #exit 1
 fi
