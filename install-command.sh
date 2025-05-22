00c08b90dec11a27fed1ba57a2f52d5087c83016
bindu <davuluribindu995@gmail.com>
2025-05-22 12:15:04 +0530

    allow all changes

--- install-command.sh
+++ install-command.sh
@@ -5,8 +5,16 @@ userid=$(id -u)
 if [ $userid -ne 0 ]
 then 
     echo "error::pls run this script with root access"
+    exit 1 #give other than 0 upto 127
 else 
      echo "you are running with root access"
 fi
 
 dnf install mysql -y
+
+if [ $? -eq 0 ]
+then 
+    echo "installation is ... success"
+else
+    echo "installation is ... failure"
+fi
